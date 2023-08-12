ea90e2fe7daef1b9eb61f0b1077d2f131324ffba
charlespasiak <charlesspasiak@gmail.com>
2023-08-12 13:33:01 +0700

    impemented google auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import { NavLinks } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from '@/lib/session';
+import ProfileMenu from './ProfileMenu';
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -22,10 +24,9 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
-            {/* <ProfileMenu session={session} /> */}
+            <ProfileMenu session={session} />
             <Link href="/create-project">
               Share Work
               {/* <Button title="Share work" /> */}
